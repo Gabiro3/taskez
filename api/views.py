35391d5880b6d31282357af333da87487cf7ef08
Gabiro3 Arnaud <gabiroarnaud@gmail.com>
2024-03-16 15:21:13 +0200

    Created API for tasks

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+
+@api_view(['GET'])
+def getTasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
