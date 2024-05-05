a7706bab256776391fbd97cef6ee86c9fe25ac58
Gabiro3 Arnaud <gabiroarnaud@gmail.com>
2024-05-05 16:30:01 +0200

    Added email return field in activity serializer

--- api/serializers.py
+++ api/serializers.py
@@ -15,7 +15,7 @@ class ActivitySerializer(serializers.ModelSerializer):
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ['id', 'name', 'avatar', 'groups', 'preferences']
+        fields = ['id', 'name', 'email' , 'avatar', 'groups', 'preferences']
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
